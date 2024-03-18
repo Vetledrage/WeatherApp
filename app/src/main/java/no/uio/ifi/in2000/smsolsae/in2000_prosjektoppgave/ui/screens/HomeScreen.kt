4bb3e12363afeefdb356012694660c8e6d373c20
Said <smsolsae@ulrik.uio.no>
2024-03-18 18:25:51 +0100

    Update HomeScreen.kt

--- app/src/main/java/no/uio/ifi/in2000/smsolsae/in2000_prosjektoppgave/ui/screens/HomeScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/smsolsae/in2000_prosjektoppgave/ui/screens/HomeScreen.kt
@@ -53,7 +53,7 @@ import no.uio.ifi.in2000.smsolsae.in2000_prosjektoppgave.ui.components.BottomBar
 //Hjemskjermen hittil.
 @Composable
 fun HomeScreen(navController: NavController){
-    val todays_temp = "14"
+    val todaystemp = "14"
 
     Surface(modifier = Modifier.fillMaxSize()) {
         Scaffold(
@@ -122,7 +122,7 @@ fun HomeScreen(navController: NavController){
                         Spacer(modifier = Modifier.height(60.dp))
 
                         Text(
-                            text = "$todays_temp°",
+                            text = "$todaystemp°",
                             fontSize = 70.sp,
                             fontWeight = FontWeight.Bold
                         )
