7595debfb49e3fc8b23590b99a119632a7473afb
Said <smsolsae@ulrik.uio.no>
2024-03-18 13:22:33 +0100

    Update getLocalTimes.kt

--- app/src/main/java/no/uio/ifi/in2000/smsolsae/in2000_prosjektoppgave/data/timeData/getLocalTimes.kt
+++ app/src/main/java/no/uio/ifi/in2000/smsolsae/in2000_prosjektoppgave/data/timeData/getLocalTimes.kt
@@ -1,2 +1,14 @@
 package no.uio.ifi.in2000.smsolsae.in2000_prosjektoppgave.data.timeData
 
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+
+
+//Returnerer nåværende dato og tid.
+
+fun getLiveDateTime(): String {
+    val calendarFormat = SimpleDateFormat("dd:MM:yyyy HH:mm:ss", Locale.getDefault())
+    var todaysDate = calendarFormat.format(Calendar.getInstance().time)
+    return todaysDate
+}
