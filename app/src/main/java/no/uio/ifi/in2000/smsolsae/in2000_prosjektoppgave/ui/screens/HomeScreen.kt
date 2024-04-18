fee874fc1f3b8cb1912bd600aa528ddba6202cb3
Said Solsaev <smsolsae@ulrik.uio.no>
2024-04-18 16:15:09 +0200

    Update HomeScreen.kt

--- app/src/main/java/no/uio/ifi/in2000/smsolsae/in2000_prosjektoppgave/ui/screens/HomeScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/smsolsae/in2000_prosjektoppgave/ui/screens/HomeScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
