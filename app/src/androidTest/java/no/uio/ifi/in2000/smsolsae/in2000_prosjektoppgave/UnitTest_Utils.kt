cdefe32c3196fa907b7ff53fca56e868f3e780e4
Said <smsolsae@ulrik.uio.no>
2024-04-19 10:41:40 +0200

    update

--- app/src/androidTest/java/no/uio/ifi/in2000/smsolsae/in2000_prosjektoppgave/UnitTest_Utils.kt
+++ app/src/androidTest/java/no/uio/ifi/in2000/smsolsae/in2000_prosjektoppgave/UnitTest_Utils.kt
@@ -1,13 +1,20 @@
 package no.uio.ifi.in2000.smsolsae.in2000_prosjektoppgave
 
-import no.uio.ifi.in2000.smsolsae.in2000_prosjektoppgave.ui.utils.DisplayImage
-import no.uio.ifi.in2000.smsolsae.in2000_prosjektoppgave.ui.utils.pickBear
-import org.junit.Assert.assertEquals
-import org.junit.Test
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
-import androidx.compose.ui.test.assertIsDisplayed
+import no.uio.ifi.in2000.smsolsae.in2000_prosjektoppgave.ui.utils.DisplayImage
+import no.uio.ifi.in2000.smsolsae.in2000_prosjektoppgave.ui.utils.formatAlertsDate
+import no.uio.ifi.in2000.smsolsae.in2000_prosjektoppgave.ui.utils.formatDate
+import no.uio.ifi.in2000.smsolsae.in2000_prosjektoppgave.ui.utils.formatTime
+import no.uio.ifi.in2000.smsolsae.in2000_prosjektoppgave.ui.utils.getDay
+import no.uio.ifi.in2000.smsolsae.in2000_prosjektoppgave.ui.utils.getLiveDateTime
+import no.uio.ifi.in2000.smsolsae.in2000_prosjektoppgave.ui.utils.pickBear
+import org.junit.Assert.assertEquals
 import org.junit.Rule
+import org.junit.Test
+
+//Test to see if we get the right bear from the temperature
 class BearPickerTests {
 
     @Test
@@ -30,9 +37,8 @@ class BearPickerTests {
 }
 
 
-
+//Testing to see if the displayed images are correct
 class DisplayImageTests {
-
     @get:Rule
     val composeTestRule = createComposeRule()
 
@@ -67,5 +73,46 @@ class DisplayImageTests {
         composeTestRule
             .onNodeWithContentDescription("Panda Bear")
             .assertIsDisplayed()
+    }
+}
+
+class TimeFormatTests {
+
+    @Test
+    fun testFormatTime() {
+        val time = "2024-04-04T04:00:00Z"
+        val expected = "04:00"
+        assertEquals(expected, formatTime(time))
+    }
+
+    @Test
+    fun testFormatDate() {
+        val date = "2024-04-04T04:00:00Z"
+        val expected = "04.04.2024"
+        assertEquals(expected, formatDate(date))
+    }
+
+    @Test
+    fun testFormatAlertsDate() {
+        val date = "2024-04-04T04:00:00+00:00"
+        val expected = "04.04.2024"
+        assertEquals(expected, formatAlertsDate(date))
+    }
+
+    @Test
+    fun testGetDay() {
+        val date = "2024-04-04T04:00:00Z"
+        val expected = "Thursday"
+        assertEquals(expected, getDay(date))
+    }
+
+    @Test
+    fun testGetLiveDateTime() {
+        // This is tricky because it uses the current date and time
+        // To test this, you might consider using a library like Mockito to mock `Calendar.getInstance()`
+        // Or adjust the test to only verify the format of the return string matches expected regex.
+        val result = getLiveDateTime()
+        val regex = "\\d{2}\\.\\w+ \\d{2}:\\d{2}".toRegex()
+        assert(result.matches(regex))
     }
 }
