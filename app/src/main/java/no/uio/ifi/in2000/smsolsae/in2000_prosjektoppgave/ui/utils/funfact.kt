2d312c87d38071be29427a498a8ba6ebbd0b87ec
Said <smsolsae@ulrik.uio.no>
2024-04-30 12:37:29 +0200

    Update funfact.kt

--- app/src/main/java/no/uio/ifi/in2000/smsolsae/in2000_prosjektoppgave/ui/utils/funfact.kt
+++ app/src/main/java/no/uio/ifi/in2000/smsolsae/in2000_prosjektoppgave/ui/utils/funfact.kt
@@ -6,8 +6,7 @@ import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
 import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
+import java.util.*
 
 
 class DailyFactManager(private val context: Context) {
