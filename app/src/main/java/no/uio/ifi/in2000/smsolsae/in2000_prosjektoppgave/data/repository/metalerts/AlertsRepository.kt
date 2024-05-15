326c0d03cd5ca8c123c7310abf2966da7b201be8
Brede <bkkvam@ulrik.uio.no>
2024-05-16 00:37:11 +0200

    Add comments to AlertsRepository.kt and ImplementedAlertsRepository.kt

--- app/src/main/java/no/uio/ifi/in2000/smsolsae/in2000_prosjektoppgave/data/repository/metalerts/AlertsRepository.kt
+++ app/src/main/java/no/uio/ifi/in2000/smsolsae/in2000_prosjektoppgave/data/repository/metalerts/AlertsRepository.kt
@@ -2,7 +2,14 @@ package no.uio.ifi.in2000.smsolsae.in2000_prosjektoppgave.data.repository.metale
 
 import no.uio.ifi.in2000.smsolsae.in2000_prosjektoppgave.ui.ui_state.AlertInfo
 
+/**
+ * Interface for the AlertsRepository. Implemented in the class ImplementedAlertsRepository.
+ */
 interface AlertsRepository {
+    /**
+     * Fetches the alert information from the data source and returns a list of AlertInfo objects.
+     * @return A mutable list of AlertInfo objects containing detailed information about alerts.
+     */
     suspend fun getAlertsInfo(): MutableList<AlertInfo>
 
 }
--- app/src/main/java/no/uio/ifi/in2000/smsolsae/in2000_prosjektoppgave/data/repository/metalerts/ImplementedAlertsRepository.kt
+++ app/src/main/java/no/uio/ifi/in2000/smsolsae/in2000_prosjektoppgave/data/repository/metalerts/ImplementedAlertsRepository.kt
@@ -3,12 +3,24 @@ package no.uio.ifi.in2000.smsolsae.in2000_prosjektoppgave.data.repository.metale
 import no.uio.ifi.in2000.smsolsae.in2000_prosjektoppgave.data.datasource.MetAlertsDataSource
 import no.uio.ifi.in2000.smsolsae.in2000_prosjektoppgave.ui.ui_state.AlertInfo
 
+/**
+ * Implementation of the AlertsRepository interface.
+ */
 class ImplementedAlertsRepository : AlertsRepository {
     private val dataSource = MetAlertsDataSource(baseUrl = "https://gw-uio.intark.uh-it.no/in2000/weatherapi")
+
+    /**
+     * Fetches the alert information from the data source and returns a list of AlertInfo objects.
+     * @return A mutable list of AlertInfo objects containing detailed information about alerts.
+     */
     override suspend fun getAlertsInfo(): MutableList<AlertInfo> {
+        //Fetch alert data from the data source
         val alert = dataSource.fetchMetAlerts()
 
+        //Initialize an empty list to store alert information
         val alertList : MutableList<AlertInfo> = mutableListOf()
+
+        //Temporary variables to hold alert properties
         var area : String?
         var type : String?
         var consequence : String?
@@ -18,9 +30,11 @@ class ImplementedAlertsRepository : AlertsRepository {
         var alertLevel: String?
         var timeInterval: List<String?>?
 
+        //Iterate over each feature in the alert data
         alert.features?.forEach{
             val prop = it.properties
 
+            //Extracting the properties from the feature
             area = prop?.area
             type = prop?.eventAwarenessName
             consequence = prop?.consequences
@@ -30,6 +44,7 @@ class ImplementedAlertsRepository : AlertsRepository {
             alertLevel = prop?.awareness_level
             timeInterval = it.time?.interval
 
+            //Create an AlertInfo object with the extracted properties
             val alertF = AlertInfo(
                 area = area,
                 type = type,
@@ -41,8 +56,10 @@ class ImplementedAlertsRepository : AlertsRepository {
                 timeInterval = timeInterval
             )
 
+            //Add the AlertInfo object to the list
             alertList.add(alertF)
         }
+        //Returning the list of alert information
         return alertList
     }
 }
