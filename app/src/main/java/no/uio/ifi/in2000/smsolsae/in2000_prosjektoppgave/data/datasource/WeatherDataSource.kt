bc679925bcff91d74cfeac025a2cd0982c978cb6
Brede <bkkvam@ulrik.uio.no>
2024-05-15 18:45:02 +0200

    Add some comments and change some comments for WeatherDataSource.kt

--- app/src/main/java/no/uio/ifi/in2000/smsolsae/in2000_prosjektoppgave/data/datasource/WeatherDataSource.kt
+++ app/src/main/java/no/uio/ifi/in2000/smsolsae/in2000_prosjektoppgave/data/datasource/WeatherDataSource.kt
@@ -16,40 +16,45 @@ import no.uio.ifi.in2000.smsolsae.in2000_prosjektoppgave.data.Model
  */
 class WeatherDataSource(val baseUrl: String){
     /**
-     * Method that calls on the data with a proxy server
-     * @param client the httpclient to use
-     * @param url The url to use
-     * @return result of a get request on the url (More information to be added)
+     * Method that makes a Get request to the server with a proxy
+     * @param client the Httpclient to use for making the request
+     * @param url The URL to send the GET request to
+     * @return HttpResponse Result of a get request on the specified URL
      */
     private suspend fun serverCall(client: HttpClient, url: String) : HttpResponse{
         return client.get(url){
             headers{
+                //Adding the necessary API for authentication
                 append("X-Gravitee-API-Key", "1c203c70-6b10-467d-9150-986d844c082b")
             }
         }
     }
 
     /**
-     * Fetches location forecast based on position
-     * @param lat latitude
-     * @param long longitude
-     * @param altitude altitude
-     * @return Model object with result of requesting weather data to the server based on location
+     * Fetches location forecast based on the coordinates provided to the function
+     * @param lat Latitude of the location
+     * @param long Longitude of the location
+     * @param altitude Altitude of the location (this parameter is optional)
+     * @return Model object containing the result of requesting weather data request
      *
      */
     suspend fun fetchLocationForecastData(lat: String, long: String, altitude: String? = null): Model {
         var cordinates = "lat=${lat}&lon=${long}"
         if (altitude != null) {
+            //Appending altitude to the coordinates if it is provided
             cordinates += "&altitude=$altitude"
         }
 
+        // Create a HttpClient instance with JSON (Gson) support, assigned to the variable called client
         val client = HttpClient{
             install(ContentNegotiation){
                 gson()
             }
         }
 
+        //Here we construct the URL for the weather data request
         val  url = "$baseUrl/locationforecast/2.0/complete?$cordinates"
+        //Make the server call and return the parsed response body
         return serverCall(client, url).body()
     }
 
