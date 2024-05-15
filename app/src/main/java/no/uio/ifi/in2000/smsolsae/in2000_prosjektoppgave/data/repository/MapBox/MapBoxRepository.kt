95e9e68e11722c3287bfaedb37ad1781e7809517
Brede <bkkvam@ulrik.uio.no>
2024-05-15 19:02:56 +0200

    Add commens to the MapBoxRepository.kt interface

--- app/src/main/java/no/uio/ifi/in2000/smsolsae/in2000_prosjektoppgave/data/repository/MapBox/MapBoxRepository.kt
+++ app/src/main/java/no/uio/ifi/in2000/smsolsae/in2000_prosjektoppgave/data/repository/MapBox/MapBoxRepository.kt
@@ -1,5 +1,14 @@
 package no.uio.ifi.in2000.smsolsae.in2000_prosjektoppgave.data.repository.MapBox
 
+/**
+ * Interface for the MapBoxRepository. Implemented in the class ImplementedMapBoxRepository
+ */
 interface MapBoxRepository {
+
+    /**
+     * Fetches the coordinates (latitude and longitude) for a given city.
+     * @param city The name of the city to get coordinates for.
+     * @return A Pair of Doubles representing the latitude and longitude, or null if the coordinates could not be found.
+     */
     suspend fun getCoordinatesForAddress(city: String): Pair<Double, Double>?
 }
